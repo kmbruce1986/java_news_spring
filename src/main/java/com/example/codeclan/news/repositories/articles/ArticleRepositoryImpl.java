73be3196f6dd9a791fce388ac5863f761a046a63
anne-other <anne.c.ince@gmail.com>
2018-10-22 16:55:43 +0100

    adding date order to articles

--- src/main/java/com/example/codeclan/news/repositories/articles/ArticleRepositoryImpl.java
+++ src/main/java/com/example/codeclan/news/repositories/articles/ArticleRepositoryImpl.java
@@ -4,10 +4,12 @@ import com.example.codeclan.news.models.Article;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManager;
+import javax.persistence.OrderBy;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -25,6 +27,7 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom{
         try {
             cr.createAlias("categories", "category");
             cr.add(Restrictions.eq("category.Id", categoryId));
+            cr.addOrder(Order.asc("publishedDateTime"));
             results = cr.list();
         } catch (HibernateException e) {
             e.printStackTrace();
@@ -42,6 +45,24 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom{
         try {
             Criteria cr = session.createCriteria(Article.class);
             cr.add(Restrictions.eq("journalist.id", journalistId));
+            results = cr.list();
+            cr.addOrder(Order.asc("publishedDateTime"));
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+    @Override
+    @Transactional
+    public List<Article> getArticles() {
+        List<Article> results = null;
+        Session session = entityManager.unwrap(Session.class);
+        Criteria cr = session.createCriteria(Article.class);
+        try {
+            cr.addOrder(Order.asc("publishedDateTime"));
             results = cr.list();
         } catch (HibernateException e) {
             e.printStackTrace();
